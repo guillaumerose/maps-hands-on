05c6ed73476a39b8f9fc8a2cf791ebce47647bc2
Ulrich VACHON <uvachon@gmail.com>
2018-04-16 22:12:58 +0200

    plan - remove unnecessary start.sh files

--- 1_plan/part0/start.sh
+++
@@ -1,4 +0,0 @@
-docker-compose rm --force
-echo "loading tileserver-gl image from ../tileserver-gl.tar.gz"
-docker load < ../tileserver-gl.tar.gz
-docker-compose up
--- 1_plan/part1/start.sh
+++
@@ -1,4 +0,0 @@
-docker-compose rm --force
-echo "loading tileserver-gl image"
-docker load < ../tileserver-gl.tar.gz
-docker-compose up
